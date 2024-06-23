19eb0e4617ac89f60cbdbed8e63df732b520c6e5
Nguyen Chi Quoc <nguyenchiquocit@gmail.com>
2024-06-23 17:18:48 +0700

    create project

---
+++ src/test/java/com/example/ShopLaptop/ShopLaptopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ShopLaptop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopLaptopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
